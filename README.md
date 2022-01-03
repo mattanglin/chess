3a9971c9524ec615c9705336cb70ace5744e85ae
Matt Anglin <mattanglin@gmail.com>
2022-01-02 22:53:10 -0800

    Subpath for pages

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Chess
 An exploration into the fundamentals of chess
+
+[Play a game](https://mattanglin.github.io/chess/)
--- next.config.js
+++ next.config.js
@@ -3,4 +3,5 @@ const withImages = require('next-images');
 
 module.exports = withImages({
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/chess' : undefined,
 })
