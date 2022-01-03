7f648e687bb8dce3ab684904b78a54aff2b9fdb0
mattanglin <mattanglin@gmail.com>
2022-01-03 22:29:53 +0000

    Deploying to gh-pages from @ mattanglin/chess@e02cf531484dfea333f1cebc163510cbf7c76e22 🚀

---
+++ _next/static/dw4oEbJ3gznZsiTzfBuQY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/950-438a89468b22ab88.js","static/chunks/pages/index-87cebf5fd08225e7.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/n3NgAvTeUejIDORRH5IJL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/950-438a89468b22ab88.js","static/chunks/pages/index-9d2c20f387b0dc8c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
