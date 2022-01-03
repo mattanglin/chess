b832fa2867c64194a43861e18a97273f95c6a077
Matt Anglin <mattanglin@gmail.com>
2022-01-02 22:36:42 -0800

    Cleanup and pages build

--- src/services/chess/status/index.ts
+++ src/services/chess/status/index.ts
@@ -1,11 +1,5 @@
 import { attacked } from './attacked';
-import { check } from './check';
-import { checkmate } from './checkmate';
-import { stalemate } from './stalemate'; 
 
 export const status = {
   attacked,
-  check,
-  checkmate,
-  stalemate,
 };
